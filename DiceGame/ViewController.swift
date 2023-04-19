2de7c103db9b69acf93331281e4553ae8e108992
haram22 <kimhr9889s@gamil.com>
2023-04-19 14:44:14 +0900

    dice game dome

--- DiceGame/ViewController.swift
+++ DiceGame/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var secondImageView: UIImageView!
+    
+    // 배열에 데이터들 저장해두고 랜덤으로 뽑아서 쓴다.
+    // UIImage라는 타입을 갖는 배열 변수
+    var diceArray: [UIImage] = [#imageLiteral(resourceName: "black1"), #imageLiteral(resourceName: "black2"), #imageLiteral(resourceName: "black3"), #imageLiteral(resourceName: "black4"), #imageLiteral(resourceName: "black5"), #imageLiteral(resourceName: "black6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func rollButtonTapped(_ sender: UIButton) {
+        // 첫번째 이미지뷰의 이미지를 랜덤으로 변경해야 함.
+        firstImageView.image = diceArray.randomElement()//랜덤 이미지
+        
+        // 두번째 이미지뷰의 이미지를 랜덤으로 변경해야 함.
+        secondImageView.image = diceArray.randomElement()//random
+    }
 }
-
